1430261a333606dd6545a98caabfe18a52c3fc9d
ISAWASHUN <syun20000616@icloud.com>
2024-01-23 21:24:00 +0900

    componentsの作り込み

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contacts from "./components/contacts";
+import Footer from "./components/Footer";
+import TopButton from "./components/TopButton";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-darkblue">Home</h1>
+    <Hero />
+    <About />
+    <Skills />
+    <Projects />
+    <Contacts />
+    <Footer />
+    <TopButton />
     </div>
   );
 }
