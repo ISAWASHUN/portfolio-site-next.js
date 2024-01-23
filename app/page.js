53f4a6bc06983ab06a5b25b8c8ac7cceb6ca7839
ISAWASHUN <syun20000616@icloud.com>
2024-01-23 21:39:17 +0900

    importの参照先の修正

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contacts from "./components/contacts";
+import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
 import TopButton from "./components/TopButton";
 
