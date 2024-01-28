f598e2f6efd4737c8415cecd23cc4cfabfb5d6b7
ISAWASHUN <syun20000616@icloud.com>
2024-01-28 14:15:28 +0900

    データの追加

---
+++ constatns/projects.js
@@ -0,0 +1,76 @@
+export const projects = [
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/1.png',
+    category:['react', 'aws']
+  },
+  {
+    name: '働きからメディア',
+    githubUrl: '',
+    image: '../public/assets/projects/2.png',
+    category:['react', 'aws']
+  },
+  {
+    name: 'マーケティング戦略',
+    githubUrl: '',
+    image: '../public/assets/projects/3.png',
+    category:['react', 'aws']
+  },
+  {
+    name: '学生む向けメディア',
+    githubUrl: '',
+    image: '../public/assets/projects/4.png',
+    category:['react'],
+    projectUrl: '',
+    deployed: true
+  },
+  {
+    name: 'ビジネスマン向けメディア',
+    githubUrl: '',
+    image: '../public/assets/projects/5.png',
+    category:['react']
+  },
+  {
+    name: 'ポートフォリオ',
+    githubUrl: '',
+    image: '../public/assets/projects/6.png',
+    category:['react', 'aws']
+  },
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/7.png',
+    category:['wordpress']
+  },
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/8.png',
+    category:['react', 'aws']
+  },
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/9.png',
+    category:['react', 'aws']
+  },
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/10.png',
+    category:['react', 'aws']
+  },
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/11.png',
+    category:['react', 'aws']
+  },
+  {
+    name: 'フリーランスホームページ',
+    githubUrl: '',
+    image: '../public/assets/projects/12.png',
+    category:['react', 'aws']
+  },
+]
